c82fbfa3a802b7878b867917c32c763b33d2ea8a
harukahirata <haruka.h@chot-inc.com>
2025-04-23 10:44:43 +0900

    add: 練習用コードを追加

---
+++ src/shopping-list.ts
@@ -0,0 +1,26 @@
+export const shoppingList = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 200
+  },
+  {
+    id: 2,
+    name: "Milk",
+    price: 200
+  },
+  {
+    id: 3,
+    name: "Meat",
+    price: 400
+  },
+  {
+    id: 4,
+    name: "Fish",
+    price: 100
+  }
+];
+
+const shoppingListPrices = shoppingList.map((item) => item.price);
+const total = shoppingListPrices.reduce((sum, price) => sum + price, 0);
+console.log(total);
