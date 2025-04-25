e568700bdcebb4b7a1d77de6b2b6d26650b0f467
harukahirata <haruka.h@chot-inc.com>
2025-04-25 17:26:37 +0900

    test: 関数getItemsByMoreThanPriceのテストコードを追加

--- src/shopping-list.test.ts
+++ src/shopping-list.test.ts
@@ -1,8 +1,21 @@
 import { describe, expect, test } from "vitest";
-import { getAllName } from './shopping-list';
+import { getItemsByMoreThanPrice } from './shopping-list';
 
-describe("getAllName", () => {
-test("出力は「Apple, Milk, Meat, Fish」", () => {
-expect(getAllName()).toBe("Apple, Milk, Meat, Fish");
+// describe("getItemsByMoreThanPrice", () => {
+// test("出力は「Apple, Milk, Meat, Fish」", () => {
+// expect(getAllName()).toBe("Apple, Milk, Meat, Fish");
+// })
+// });
+
+describe("getItemsByMoreThanPrice", () => {
+  test("priceが0以下のときは空の配列を返す", () => {
+    expect(getItemsByMoreThanPrice(0)).toEqual([]);
+  })
+  test("priceが200のとき、200円以上の商品（オブジェクト）を返す", () => {
+    expect(getItemsByMoreThanPrice(200)).toEqual([
+      { id: 1, name: 'Apple', price: 200 },
+      { id: 2, name: 'Milk', price: 200 },
+      { id: 3, name: 'Meat', price: 400 }
+    ])
+  })
 })
-});
