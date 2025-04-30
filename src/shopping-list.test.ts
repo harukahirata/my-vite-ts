482c0092829849ece855b6cbf1cea196d3586331
harukahirata <haruka.h@chot-inc.com>
2025-04-30 16:29:43 +0900

    test: 追加課題2のテストコードを追加

--- src/shopping-list.test.ts
+++ src/shopping-list.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { getItemsByMoreThanPrice } from './shopping-list';
+import { getItemByName } from './shopping-list';
 
 // describe("getItemsByMoreThanPrice", () => {
 // test("出力は「Apple, Milk, Meat, Fish」", () => {
@@ -7,21 +7,33 @@ import { getItemsByMoreThanPrice } from './shopping-list';
 // })
 // });
 
-describe("getItemsByMoreThanPrice", () => {
-  test("priceが0以下のときは空の配列を返す", () => {
-    expect(getItemsByMoreThanPrice(0)).toEqual([]);
+// describe("getItemsByMoreThanPrice", () => {
+//   test("priceが0以下のときは空の配列を返す", () => {
+//     expect(getItemsByMoreThanPrice(0)).toEqual([]);
+//   })
+//   test("priceが200のとき、200円以上の商品（オブジェクト）を返す", () => {
+//     expect(getItemsByMoreThanPrice(200)).toEqual([
+//       { id: 1, name: 'Apple', price: 200 },
+//       { id: 2, name: 'Milk', price: 200 },
+//       { id: 3, name: 'Meat', price: 400 }
+//     ])
+//   })
+//   test("priceが-1のとき、空の配列を返す", () => {
+//     expect(getItemsByMoreThanPrice(-1)).toEqual([]);
+//   })
+//   test("priceがNaNのとき、空の配列を返す", () => {
+//     expect(getItemsByMoreThanPrice(NaN)).toEqual([]);
+//   })
+// });
+
+describe("getItemByName", () => {
+  test("nameがFishのとき、nameにFishを含んだオブジェクトを返す", () => {
+    expect(getItemByName("Fish")).toStrictEqual({ id: 4, name: 'Fish', price: 100 });
   })
-  test("priceが200のとき、200円以上の商品（オブジェクト）を返す", () => {
-    expect(getItemsByMoreThanPrice(200)).toEqual([
-      { id: 1, name: 'Apple', price: 200 },
-      { id: 2, name: 'Milk', price: 200 },
-      { id: 3, name: 'Meat', price: 400 }
-    ])
+  test("nameが文字列以外のとき、エラーメッセージを返す", () => {
+    expect(getItemByName(123 as unknown as string)).toBe("文字を入れてください");
   })
-  test("priceが-1のとき、空の配列を返す", () => {
-    expect(getItemsByMoreThanPrice(-1)).toEqual([]);
+  test("nameに当てはまらない場合、undefinedを返す", () => {
+    expect(getItemByName("あいう")).toBe(undefined);
   })
-  test("priceがNaNのとき、空の配列を返す", () => {
-    expect(getItemsByMoreThanPrice(NaN)).toEqual([]);
-  })
-});
+})
