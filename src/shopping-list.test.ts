a57c90a8a34eace95e614c4f145f52bc0e6f319f
harukahirata <haruka.h@chot-inc.com>
2025-05-01 09:34:30 +0900

    add: コメントアウトを解除・skipの処理を追加

--- src/shopping-list.test.ts
+++ src/shopping-list.test.ts
@@ -1,30 +1,32 @@
 import { describe, expect, test } from "vitest";
+import { getAllName } from './shopping-list';
+import { getItemsByMoreThanPrice } from './shopping-list';
 import { getItemByName } from './shopping-list';
 
-// describe("getItemsByMoreThanPrice", () => {
-// test("出力は「Apple, Milk, Meat, Fish」", () => {
-// expect(getAllName()).toBe("Apple, Milk, Meat, Fish");
-// })
-// });
+describe.skip("getAllName", () => {
+test("出力は「Apple, Milk, Meat, Fish」", () => {
+expect(getAllName()).toBe("Apple, Milk, Meat, Fish");
+})
+});
 
-// describe("getItemsByMoreThanPrice", () => {
-//   test("priceが0以下のときは空の配列を返す", () => {
-//     expect(getItemsByMoreThanPrice(0)).toEqual([]);
-//   })
-//   test("priceが200のとき、200円以上の商品（オブジェクト）を返す", () => {
-//     expect(getItemsByMoreThanPrice(200)).toEqual([
-//       { id: 1, name: 'Apple', price: 200 },
-//       { id: 2, name: 'Milk', price: 200 },
-//       { id: 3, name: 'Meat', price: 400 }
-//     ])
-//   })
-//   test("priceが-1のとき、空の配列を返す", () => {
-//     expect(getItemsByMoreThanPrice(-1)).toEqual([]);
-//   })
-//   test("priceがNaNのとき、空の配列を返す", () => {
-//     expect(getItemsByMoreThanPrice(NaN)).toEqual([]);
-//   })
-// });
+describe.skip("getItemsByMoreThanPrice", () => {
+  test("priceが0以下のときは空の配列を返す", () => {
+    expect(getItemsByMoreThanPrice(0)).toEqual([]);
+  })
+  test("priceが200のとき、200円以上の商品（オブジェクト）を返す", () => {
+    expect(getItemsByMoreThanPrice(200)).toEqual([
+      { id: 1, name: 'Apple', price: 200 },
+      { id: 2, name: 'Milk', price: 200 },
+      { id: 3, name: 'Meat', price: 400 }
+    ])
+  })
+  test("priceが-1のとき、空の配列を返す", () => {
+    expect(getItemsByMoreThanPrice(-1)).toEqual([]);
+  })
+  test("priceがNaNのとき、空の配列を返す", () => {
+    expect(getItemsByMoreThanPrice(NaN)).toEqual([]);
+  })
+});
 
 describe("getItemByName", () => {
   test("nameがFishのとき、nameにFishを含んだオブジェクトを返す", () => {
