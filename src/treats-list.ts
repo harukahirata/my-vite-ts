db4e81932be059f97a7899aa341dde3ccb36f56d
harukahirata <haruka.h@chot-inc.com>
2025-05-07 12:19:11 +0900

    add: 配列と関数getAffordableTreatsを追加

---
+++ src/treats-list.ts
@@ -0,0 +1,33 @@
+type treat = {
+  name: string;
+  price: number;
+  count: number;
+};
+
+const treats = [
+  { name: "チョコレート", price: 120, count: 1 },
+  { name: "ポテトチップス", price: 110, count: 1 },
+  { name: "ラムネ", price: 100, count: 1 },
+] satisfies treat[];
+
+const limitPrice = 300;
+
+export function getAffordableTreats() {
+  const total = treats.reduce((sum, item) => sum + item.price, 0);
+  if (total <= limitPrice) {
+    console.log("合計金額は300円以下です。");
+  } else if (total > limitPrice) {
+    // 配列treatsの中からpriceの最大値を求める
+    const maxPrice = Math.max(...treats.map(item => item.price));
+    // 配列treatsの中からpriceの最大値maxPriceに一致する一番目のオブジェクトのインデックスを求める
+    const index = treats.findIndex(item => item.price === maxPrice);
+    // 対象のインデックスのみを除いた配列を作る
+    const newTreats = [
+      ...treats.slice(0, index),
+      ...treats.slice(index + 1),
+    ];
+    return newTreats;
+  }
+}
+const shoppingBasket = getAffordableTreats();
+console.log(shoppingBasket);
