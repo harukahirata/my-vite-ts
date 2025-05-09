7709753638ac092214c406378c83be09d4402d0c
harukahirata <haruka.h@chot-inc.com>
2025-05-09 14:52:08 +0900

    test: validateTreat関数のテストケースを追加

--- src/treats-list.test.ts
+++ src/treats-list.test.ts
@@ -7,10 +7,16 @@ describe("validateTreat", () => {
     expect(validateTreat({ name: "チョコレート", price: 120, count: 1 })).toBe(true);
   });
   test("countが0のためfalseを返す", () => {
-    expect(validateTreat({ name: "クッキー", price: 100, count: 0})).toBe(false)
+    expect(validateTreat({ name: "クッキー", price: 100, count: 0})).toBe(false);
+  });
+  test("countが-1のためfalseを返す", () => {
+    expect(validateTreat({ name: "クッキー", price: 100, count: -1})).toBe(false);
+  });
+  test("priceが0のためfalseを返す", () => {
+    expect(validateTreat({ name: "ガム", price: 0, count: 1})).toBe(false);
   });
   test("priceが-1のためfalseを返す", () => {
-    expect(validateTreat({ name: "ガム", price: -1, count: 1})).toBe(false)
+    expect(validateTreat({ name: "ガム", price: -1, count: 1})).toBe(false);
   });
 });
 
