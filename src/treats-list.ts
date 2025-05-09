fcdf6b9b3f25c81f5c819630d4cd9449d2fbc196
harukahirata <haruka.h@chot-inc.com>
2025-05-09 11:48:56 +0900

    add: 配列の値をチェックする関数を追加・戻り値の型を指定

--- src/treats-list.ts
+++ src/treats-list.ts
@@ -1,21 +1,27 @@
-type treat = {
+type Treat = {
   name: string;
   price: number;
   count: number;
 };
 
+export function validateTreat(treat: Treat): boolean {
+  return treat.price > 0 && treat.count > 0;
+}
+
 const treats = [
   { name: "チョコレート", price: 120, count: 1 },
   { name: "ポテトチップス", price: 110, count: 1 },
   { name: "ラムネ", price: 100, count: 1 },
-] satisfies treat[];
+  { name: "クッキー", price: 100, count: 0}
+].filter(validateTreat) satisfies Treat[];
 
 const limitPrice = 300;
 
-export function getAffordableTreats() {
+export function getAffordableTreats(): Treat[] | void {
   const total = treats.reduce((sum, item) => sum + item.price, 0);
   if (total <= limitPrice) {
-    return console.log("合計金額は" + limitPrice + "円以下です。");
+    console.log("合計金額は" + limitPrice + "円以下です。");
+    return;
   }
   if (total > limitPrice) {
     // 配列treatsの中からpriceの最大値を求める
