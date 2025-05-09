aba0e69967ae7cd9e064e4e764906ca5a83b3931
harukahirata <haruka.h@chot-inc.com>
2025-05-09 11:49:23 +0900

    test: 配列の値をチェックする関数のテストを追加

--- src/treats-list.test.ts
+++ src/treats-list.test.ts
@@ -1,5 +1,18 @@
 import { describe, expect, test } from "vitest";
 import { getAffordableTreats } from './treats-list';
+import { validateTreat } from './treats-list';
+
+describe("validateTreat", () => {
+  test("priceとcountが両方とも正のためtrueを返す", () => {
+    expect(validateTreat({ name: "チョコレート", price: 120, count: 1 })).toBe(true);
+  });
+  test("countが0のためfalseを返す", () => {
+    expect(validateTreat({ name: "クッキー", price: 100, count: 0})).toBe(false)
+  });
+  test("priceが-1のためfalseを返す", () => {
+    expect(validateTreat({ name: "ガム", price: -1, count: 1})).toBe(false)
+  });
+});
 
 describe("getAffordableTreats", () => {
   test("チョコレートを除いたものが出力される", () => {
